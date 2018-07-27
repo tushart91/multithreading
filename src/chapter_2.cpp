e22bb4b4e856309fdab36023da5cee911dbf2e10
Tushar Tiwari <tushart91@gmail.com>
2018-07-27 01:35:58 -0400

    Add chapter 2

--- src/chapter_2.cpp
+++ src/chapter_2.cpp
@@ -7,12 +7,31 @@
 #include <thread>
 #include <mutex>
 #include <future>
+#include <numeric>
 
 
 using namespace std;
 
+namespace {
+    const int MIN_PER_THREAD = 25;
+}
+
+
+//template<typename InputIt, typename T>
+//struct block_parallel {
+//    void operator()(InputIt first, InputIt last, T init) {
+//        accumulate(first, last, init);
+//    }
+//};
+
+template<typename InputIt, typename T>
+T parallel_accumulate(InputIt first, InputIt last, T init) {
+    // Perform accumulate in parallel by using threads.
+    // The no of threads used will come from
+    // thread::hardware_concurrency()
+
+}
 
 TEST(Chapter2, Init) { /* NOLINT */
 
-    cout << thread::hardware_concurrency() << endl;
 }
